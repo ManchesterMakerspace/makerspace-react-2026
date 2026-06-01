b966739fb6bc5c1c5f474aaaf3542b595af68884
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-05-31 21:53:24 -0400

    Add lazy loading for route components
    Wrap routes in React.Suspense to enable code splitting.

--- src/app/PublicRouting.tsx
+++ src/app/PublicRouting.tsx
@@ -2,24 +2,30 @@ import * as React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { Routing } from 'app/constants';
-import LandingPage from 'pages/registration/LandingPage';
-import { SignUpWorkflow } from 'pages/registration/SignUpWorkflow/SignUpWorkflow';
-import PasswordReset from 'ui/auth/PasswordReset';
-import LoginPage from 'ui/auth/LoginPage';
-import UnsubscribeEmails from 'ui/member/UnsubscribeEmails';
-import FirebaseCallback from 'ui/auth/FirebaseCallback';
+import LoadingOverlay from 'ui/common/LoadingOverlay';
+
+const LandingPage = React.lazy(() => import('pages/registration/LandingPage'));
+const SignUpWorkflow = React.lazy(() =>
+  import('pages/registration/SignUpWorkflow/SignUpWorkflow').then(module => ({ default: module.SignUpWorkflow }))
+);
+const PasswordReset = React.lazy(() => import('ui/auth/PasswordReset'));
+const LoginPage = React.lazy(() => import('ui/auth/LoginPage'));
+const UnsubscribeEmails = React.lazy(() => import('ui/member/UnsubscribeEmails'));
+const FirebaseCallback = React.lazy(() => import('ui/auth/FirebaseCallback'));
 
 const PublicRouting: React.FC<{}> = () => {
   return (
-    <Routes>
-      <Route path={`${Routing.PasswordReset}/:token`} element={<PasswordReset />} />
-      <Route path={Routing.Login} element={<LoginPage />} />
-      <Route path={Routing.SignUp} element={<SignUpWorkflow />} />
-      <Route path={Routing.Root} element={<LandingPage />} />
-      <Route path={Routing.Unsubscribe} element={<UnsubscribeEmails />} />
-      <Route path='/auth/callback' element={<FirebaseCallback />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <React.Suspense fallback={<LoadingOverlay id="route-loading" />}>
+      <Routes>
+        <Route path={`${Routing.PasswordReset}/:token`} element={<PasswordReset />} />
+        <Route path={Routing.Login} element={<LoginPage />} />
+        <Route path={Routing.SignUp} element={<SignUpWorkflow />} />
+        <Route path={Routing.Root} element={<LandingPage />} />
+        <Route path={Routing.Unsubscribe} element={<UnsubscribeEmails />} />
+        <Route path='/auth/callback' element={<FirebaseCallback />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </React.Suspense>
   );
 };
 
